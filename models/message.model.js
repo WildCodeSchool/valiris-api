d2e214ee706c9fd9d2ee9ff2b8f98ca9b22d4851
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-06-10 17:11:03 +0200

    implements second route

---
+++ models/message.model.js
@@ -0,0 +1,25 @@
+const db = require('../db.js');
+const Joi = require('@hapi/joi');
+
+class Message {
+  contructor (message) {
+    this.content = message.content;
+  }
+
+  static validate (attributes) {
+    const schema = Joi.object({
+      content: Joi.string().required()
+    });
+    return schema.validate(attributes);
+  }
+
+  static async create (newMessage) {
+    return db.query('INSERT INTO message SET ?', newMessage)
+      .then(res => {
+        newMessage.id = res.insertId;
+        return newMessage;
+      });
+  }
+}
+
+module.exports = Message;
--- server.js
+++ server.js
@@ -16,6 +16,8 @@ if (process.env.NODE_ENV !== 'production') {
 
 app.use('/contacts', require('./routes/contact.routes.js'));
 
+app.use('/messages', require('./routes/message.routes.js'));
+
 // set port, listen for requests
 const server = app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
