2cc2ac841007914b19615c1769bf07816dee1e67
K0Si-003 <hugo.pioda@gmail.com>
2020-06-11 11:16:44 +0200

    check if contact exist2

--- controllers/from.controller.js
+++ controllers/from.controller.js
@@ -19,19 +19,18 @@ class formController {
     return res.status(201).send(newContact);
   }
 
-  static async createMessage (req, res) {
-    const clientPayload = req.body;
-    const { error } = Message.validate(clientPayload);
-    console.log(clientPayload);
-    if (error) {
-      console.log(JSON.stringify(error));
-      return res.status(422).send({ errorMessage: error.message, errorDetails: error.details });
-    } else {
-      const newMessage = await Message.createMessage(clientPayload);
-      return res.status(201).send(newMessage);
-    }
-  }
-
+  // static async createMessage (req, res) {
+  //   const clientPayload = req.body;
+  //   const { error } = Message.validate(clientPayload);
+  //   console.log(clientPayload);
+  //   if (error) {
+  //     console.log(JSON.stringify(error));
+  //     return res.status(422).send({ errorMessage: error.message, errorDetails: error.details });
+  //   } else {
+  //     const newMessage = await Message.createMessage(clientPayload);
+  //     return res.status(201).send(newMessage);
+  //   }
+  // }
 }
 
 module.exports = formController;
