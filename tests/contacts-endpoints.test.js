6d1d54c628e80a869953799b54732f4fdef3acbe
vbouault <vbouault@hotmail.fr>
2020-06-12 10:17:50 +0200

    fix test

--- tests/contacts-endpoints.test.js
+++ tests/contacts-endpoints.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../server.js');
 const _ = require('lodash');
 
-describe('contact endpoints', () => {
+describe('form endpoints', () => {
   describe('POST /forms', () => {
     describe('when the payload is valid', () => {
       it('returns 201 status code and the updated object when everything is provided', async () => {
@@ -10,14 +10,15 @@ describe('contact endpoints', () => {
           lastname: 'Trapet',
           firstname: 'Lancelot',
           email: 'lens78@gmail.com',
-          phone: '600000000'
+          phone: '600000000',
+          message: 'hello world'
         };
         return request(app).post('/forms').send(validContact).expect(201).then(res => {
           expect(_.isPlainObject(res.body));
           expect(res.body).toHaveProperty('lastname');
           expect(res.body).toHaveProperty('email');
+          expect(res.body).toHaveProperty('content');
           expect(res.body.firstname).toBe('Lancelot');
-          // expect(_.isNumber(res.body.phone));
         });
       });
     });
