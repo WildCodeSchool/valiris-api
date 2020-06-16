f6bfe2f69c7e36c01ab3b26cdcd0ac080aa3b9dc
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-06-16 17:04:21 +0200

    implements route GET/apartment/:id

--- models/apartment.model.js
+++ models/apartment.model.js
@@ -2,10 +2,10 @@ const db = require('../db.js');
 const Joi = require('@hapi/joi');
 
 class Apartment {
-    static async findById(id) {
-        return db.query('SELECT * FROM apartment JOIN secondary_picture ON apartment.id = secondary_picture.id_apartment WHERE apartment.id = ?', [parseInt(id, 10)])
-        .then(rows => rows[0] ? rows[0] : null);
-    }
+  static async findById (id) {
+    return db.query('SELECT * FROM apartment JOIN secondary_picture ON apartment.id = secondary_picture.id_apartment WHERE apartment.id = ?', [parseInt(id, 10)])
+      .then(rows => rows[0] ? rows[0] : null);
+  }
 }
 
 module.exports = Apartment;
