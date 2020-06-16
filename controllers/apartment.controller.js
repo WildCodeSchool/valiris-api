6f7258f5c38fbca22f44834e5e79db1292025813
vbouault <vbouault@hotmail.fr>
2020-06-16 17:03:09 +0200

    create route

--- controllers/apartment.controller.js
+++ controllers/apartment.controller.js
@@ -1,10 +1,9 @@
 const Apartment = require('../models/apartment.model.js');
 
 class ApartmentController {
-
   static async findAll (req, res) {
     try {
-      const data = await Apartment.getAll()
+      const data = await Apartment.getAll();
       res.status(200).send(data);
     } catch (err) {
       res.status(500).send({
--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -33,7 +33,7 @@ class CustomersController {
         email: c.email,
         active: !!c.active
       }));
-      
+
       res.send({ data: [] });
     } catch (err) {
       res.status(500).send({
