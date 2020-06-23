1b4e20a1174950ceb881bd362999b8577695a860
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-06-23 11:02:17 +0200

    adds camel case and changes routes

--- routes/apartment.routes.js
+++ routes/apartment.routes.js
@@ -2,7 +2,7 @@ const apartmentsController = require('../controllers/apartment.controller.js');
 const availabilitiesController = require('../controllers/availabilities.controller.js');
 const router = require('express').Router();
 
-router.get('/:id/:lang', apartmentsController.findOne);
+router.get('/:id/language/:lang', apartmentsController.findOne);
 router.get('/:id/availabilities', availabilitiesController.findAll);
 router.get('/:lang', apartmentsController.findAll);
 
