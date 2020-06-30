cf5a0a776df42b8ef87c21d664811468c2f0687f
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-06-30 12:19:13 +0200

    adds request for booking table

--- models/booking.model.js
+++ models/booking.model.js
@@ -4,6 +4,16 @@ class Booking {
   static async getAll () {
     return db.query('SELECT c.firstname, c.lastname, b.starting_date, b.ending_date FROM booking b JOIN contact c ON c.id = b.id_contact WHERE validation = 0')
   }
+
+  static async createBooking (newBooking, contactId) {
+    const fullBooking = { ...newBooking, id_contact: contactId, validation: 0 };
+    return db.query('INSERT INTO booking SET ?', fullBooking)
+      .then(res => {
+        newBooking.id = res.insertId;
+        newBooking.id_contact = contactId;
+        return newBooking;
+      });
+  }
 }
 
 module.exports = Booking;
