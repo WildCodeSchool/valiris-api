27e48bc9fb22e701d55b589f72254dc0cdb4cc57
vbouault <vbouault@hotmail.fr>
2020-07-05 10:19:33 +0200

    route getall availabilities

--- models/availabilities.model.js
+++ models/availabilities.model.js
@@ -5,6 +5,21 @@ class Availabilities {
     return db.query('SELECT apartment.name, booking.id, DATE_FORMAT(starting_date, "%Y-%m-%d") as starting_date, DATE_FORMAT(booking.ending_date, "%Y-%m-%d") as ending_date, id_apartment FROM booking JOIN apartment ON apartment.id = booking.id_apartment WHERE id_apartment = ?', [apartmentId])
       .then(rows => rows || null);
   }
+
+  static async getAll () {
+    return db.query(`
+    SELECT 
+    apartment.name, 
+    booking.id, 
+    DATE_FORMAT(starting_date, "%Y-%m-%d") as starting_date, 
+    DATE_FORMAT(booking.ending_date, "%Y-%m-%d") as ending_date, 
+    id_apartment, 
+    contact.firstname, 
+    contact.lastname 
+    FROM booking JOIN apartment ON apartment.id = booking.id_apartment 
+    JOIN contact ON contact.id = booking.id_contact`)
+    .then(rows => rows || null );
+  }
 }
 
 module.exports = Availabilities;
