b4b327df8ccd9098e65b09a9ecbdc520090d252b
vbouault <vbouault@hotmail.fr>
2020-07-06 15:17:53 +0200

    test qui ne passent toujours pas

--- tests/contacts-endpoints.test.js
+++ tests/contacts-endpoints.test.js
@@ -2,19 +2,28 @@ const request = require('supertest');
 const app = require('../server.js');
 const _ = require('lodash');
 
-describe('GET /contacts/new', () => {
-  it('returns 201 status code and the updated object when everything is provided', async () => {
-    const validContact = {
-      lastname: 'Trapet',
-      firstname: 'Lancelot',
-      email: 'lens78@gmail.com',
-      phone: '600000000'
-    };
-    return request(app).post('contacts/new').send(validContact).then(res => {
-      expect(_.isPlainObject(res.body));
-      expect(res.body).toHaveProperty('lastname');
-      expect(res.body).toHaveProperty('email');
-      expect(res.body.firstname).toBe('Lancelot');
+describe('contact endpoints', () => {
+  describe('POST /contacts', () => {
+    describe('when the payload is valid', () => {
+      it('returns 201 status code and the updated object when everything is provided', async () => {
+        const validContact = {
+          lastname: 'Trapet',
+          firstname: 'Lancelot',
+          email: 'lens78@gmail.com',
+          phone: '600000000',
+          message: 'hello world',
+          apartment: '3',
+          startDate: '2020-07-03',
+          endDate: '2020-07-17'
+        };
+        return request(app).post('/contacts').send(validContact).expect(201).then(res => {
+          expect(_.isPlainObject(res.body));
+          expect(res.body).toHaveProperty('lastname');
+          expect(res.body).toHaveProperty('email');
+          expect(res.body).toHaveProperty('content');
+          expect(res.body.firstname).toBe('Lancelot');
+        });
+      });
     });
   });
 });
