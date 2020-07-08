cee49c9065093a1499143ec9b0e67b794747e930
nathanguillaumin <61092279+nathanguillaumin@users.noreply.github.com>
2020-07-08 08:40:34 +0200

    Calendar/crud (#12)
    * change mailer
    
    * change mailer
    
    * ajout findByEmail
    
    * fixe route
    
    * change migration
    
    * fix model and controller for apartment details
    
    * fix query for apartment details
    
    * fix query for apartment details
    
    * ??
    
    * ??
    
    * ajout traduction
    
    * adds camel case
    
    * adds camel case and changes routes
    
    * adds camel case and changes routes
    
    * ??
    
    * resolves test with POST /form route
    
    * changes route into /forms/fr for post form test
    
    * removes test
    
    * Create contacts-endpoints.test.js
    
    * localstorage, accept-language,...
    
    * fix test error GH
    
    * fix test error GH
    
    * try to fix test error
    
    * adds condition for the test
    
    * delete mailer controller and adds a condition
    
    * base auth ok
    
    * base auth ok
    
    * fix err
    
    * pull us3bis
    
    * creates booking route and controller
    
    * adds booking controller
    
    * implements get request on booking
    
    * adds request for booking table
    
    * changes model and controller of booking
    
    * trying to migrate
    
    * trying to migrate
    
    * add routes contact
    
    * add routes contact
    
    * supp nom de la BDD dans migration
    
    * updates getAll query in booking model
    
    * create route contact
    
    * adds dependencies
    
    * ajout route getAll appartement back
    
    * route appartement
    
    * test github
    
    * test github
    
    * fix bug secondary, try file upload
    
    * clean comment
    
    * test upload
    
    * test upload
    
    * file upload, fix booking card home
    
    * pull contact
    
    * test
    
    * change test
    
    * change test
    
    * change test
    
    * try fixing bad test
    
    * try fixing bad test
    
    * new booking
    
    * migration foreign keys booking
    
    * route getall availabilities
    
    * test qui ne marche toujours pas
    
    * test qui ne marche toujours pas
    
    * route getOne booking
    
    * route getOne booking
    
    * test qui ne passent toujours pas
    
    * test qui ne passent toujours pas
    
    * fixes tests
    
    * implements new routes
    
    * route path booking
    
    * updates test
    
    * removes foreign key files
    
    Co-authored-by: K0Si-003 <hugo.pioda@gmail.com>
    Co-authored-by: vbouault <vbouault@hotmail.fr>
    Co-authored-by: Pierre Genthon <pierre.genthon@wildcodeschool.fr>

--- routes/apartment.routes.js
+++ routes/apartment.routes.js
@@ -5,7 +5,8 @@ const handleImageUpload = require('../middlewares/handleImageUpload.js');
 
 router.get('/:id', apartmentsController.findOne);
 router.get('/:id/back', apartmentsController.findOneBack);
-router.get('/:id/availabilities', availabilitiesController.findAll);
+router.get('/availabilities/all', availabilitiesController.findAll);
+router.get('/:id/availabilities', availabilitiesController.findAllById);
 router.get('/', apartmentsController.findAll);
 router.post('/', handleImageUpload, apartmentsController.create);
 
--- routes/booking.routes.js
+++ routes/booking.routes.js
@@ -2,6 +2,11 @@ const bookingController = require('../controllers/booking.controller.js');
 const router = require('express').Router();
 
 router.get('/', bookingController.findAll);
+router.get('/:id', bookingController.findOne);
+router.post('/', bookingController.create);
+router.get('/requests/infos', bookingController.findAllInfos);
 router.patch('/', bookingController.validateOne);
+router.patch('/:id', bookingController.updateOne);
+router.delete('/:id', bookingController.delete);
 
 module.exports = router;
