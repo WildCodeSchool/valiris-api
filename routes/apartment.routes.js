5bb250fd87707ed57938a73474dce230cb81006e
K0Si-003 <hugo.pioda@gmail.com>
2020-07-17 14:51:15 +0200

    add semicolon

--- routes/apartment.routes.js
+++ routes/apartment.routes.js
@@ -12,7 +12,7 @@ router.patch('/:id', apartmentsController.update);
 router.patch('/:id/updateSecondary', apartmentsController.updateSecondary);
 router.post('/:id/updateNewSecondary', apartmentsController.updateNewSecondary);
 router.delete('/:id', apartmentsController.delete);
-router.delete('/secondary/:id', apartmentsController.deleteSecondary)
+router.delete('/secondary/:id', apartmentsController.deleteSecondary);
 
 router.get('/:id/availabilities', availabilitiesController.findAllById);
 router.get('/availabilities/all', availabilitiesController.findAll);
