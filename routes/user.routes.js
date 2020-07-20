a91a44cf477e62076c333e9cd8a4050a499492fb
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-07-20 10:36:03 +0200

    implements requiredAuth on apartments routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,7 @@
 const usersController = require('../controllers/user.controller.js');
 const router = require('express').Router();
 
+
 router.post('/', usersController.create);
 router.get('/:id', usersController.findOne);
 router.patch('/:id', usersController.update);
