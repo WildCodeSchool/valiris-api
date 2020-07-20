868419158dd883854c2e7180e4ca48867614545d
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-07-20 10:40:18 +0200

    implements route securization on every route

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -1,11 +1,12 @@
 const contactController = require('../controllers/contact.controller.js');
 const router = require('express').Router();
+const requireAuth = require('../middlewares/requireAuth.js');
 
 router.post('/', contactController.createForm);
-router.post('/new', contactController.createContact);
-router.get('/', contactController.findAll);
-router.get('/:id', contactController.findOne);
-router.patch('/:id', contactController.update);
-router.delete('/:id', contactController.delete);
+router.post('/new', requireAuth, contactController.createContact);
+router.get('/', requireAuth, contactController.findAll);
+router.get('/:id', requireAuth, contactController.findOne);
+router.patch('/:id', requireAuth, contactController.update);
+router.delete('/:id', requireAuth, contactController.delete);
 
 module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,10 @@
 const usersController = require('../controllers/user.controller.js');
 const router = require('express').Router();
-
+const requireAuth = require('../middlewares/requireAuth.js');
 
 router.post('/', usersController.create);
-router.get('/:id', usersController.findOne);
-router.patch('/:id', usersController.update);
-router.patch('/:id/password', usersController.updatePassword);
+router.get('/:id', requireAuth, usersController.findOne);
+router.patch('/:id', requireAuth, usersController.update);
+router.patch('/:id/password', requireAuth, usersController.updatePassword);
 
 module.exports = router;
