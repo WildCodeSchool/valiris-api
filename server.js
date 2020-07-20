850c54356120724b9414cb9ebc23144348294477
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-07-20 12:19:27 +0200

    adds app.use with extract token for securization

--- server.js
+++ server.js
@@ -32,28 +32,13 @@ app.use('/uploads', express.static('uploads'));
 if (process.env.NODE_ENV !== 'production') {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
+app.use(extractToken);
 
 app.use(extractClientLanguage);
 app.use('/contacts', require('./routes/contact.routes.js'));
 app.use('/apartments', require('./routes/apartment.routes.js'));
 app.use('/bookings', require('./routes/booking.routes.js'));
 
-app.use(extractToken);
-app.use('/users', require('./routes/user.routes.js'));
-app.use('/auth', require('./routes/auth.routes.js'));
-
-app.use((err, req, res, next) => {
-  if (err.name === 'UnauthorizedError') {
-    res.status(401).send('invalid token...');
-  }
-});
-app.use((error, req, res, next) => {
-  console.error(error.stack);
-  res.status(500).send('Something Broke!');
-});
-app.set('x-powered-by', false);
-
-app.use(extractToken);
 app.use('/users', require('./routes/user.routes.js'));
 app.use('/auth', require('./routes/auth.routes.js'));
 
