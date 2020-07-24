ef6b6c1a995f272508e88b2e6b4603bebac5c637
K0Si-003 <hugo.pioda@gmail.com>
2020-07-24 12:15:31 +0200

    test apartment

--- tests/contacts-endpoints.test.js
+++ tests/contacts-endpoints.test.js
@@ -1,10 +1,22 @@
 const request = require('supertest');
 const app = require('../server.js');
+const Apartment = require('../models/apartment.model.js');
 const _ = require('lodash');
 
 describe('apartment endpoints', () => {
   describe('Get /apartments/:id', () => {
     describe('when the payload is valid', () => {
+      Apartment.create({
+        name: 'Test apartment',
+        week_price: 220,
+        month_price: 440,
+        details_fr: 'details fr',
+        details_en: 'details en',
+        title_fr: 'title fr',
+        title_en: 'title en',
+        main_picture_url: 'main_picture'
+      });
+
       it('returns 200 status code and the list of apartment', async () => {
         return request(app).get('/apartments/1').expect(200).then(res => {
           expect(_.isPlainObject(res.body));
