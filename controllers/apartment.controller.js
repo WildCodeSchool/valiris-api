d5698ebf695d1430bd4f1b29f9bb183930c876ed
K0Si-003 <hugo.pioda@gmail.com>
2020-07-24 10:56:48 +0200

    delete console.log

--- controllers/apartment.controller.js
+++ controllers/apartment.controller.js
@@ -44,7 +44,6 @@ class ApartmentController {
 
   static async upload (req, res) {
     try {
-      console.log('uploaded file : ', req.file);
       const currentPicture = req.file ? 'uploads/' + req.file.filename : null;
       res.status(200).send(currentPicture);
     } catch (err) {
