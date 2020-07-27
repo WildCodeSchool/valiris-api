9c8845b1f4a917d64f99559ae6bf28481fa34d02
K0Si-003 <hugo.pioda@gmail.com>
2020-07-27 11:13:59 +0200

    update test

--- tests/contacts-endpoints.test.js
+++ tests/contacts-endpoints.test.js
@@ -6,19 +6,22 @@ const _ = require('lodash');
 describe('apartment endpoints', () => {
   describe('Get /apartments/:id', () => {
     describe('when the payload is valid', () => {
-      Apartment.create({
-        name: 'Test apartment',
-        week_price: 220,
-        month_price: 440,
-        details_fr: 'details fr',
-        details_en: 'details en',
-        title_fr: 'title fr',
-        title_en: 'title en',
-        main_picture_url: 'main_picture'
-      });
+      let createdApartment = null;
+      beforeEach(async () => { 
+        createdApartment = await Apartment.create({
+          name: 'Test apartment',
+          week_price: 220,
+          month_price: 440,
+          details_fr: 'details fr',
+          details_en: 'details en',
+          title_fr: 'title fr',
+          title_en: 'title en',
+          main_picture_url: 'main_picture'
+        });
+      })
 
       it('returns 200 status code and the list of apartment', () => {
-        return request(app).get('/apartments/1').expect(200).then(res => {
+        return request(app).get(`/apartments/${createdApartment.id}`).expect(200).then(res => {
           expect(_.isPlainObject(res.body));
         });
       });
