c4044fe0deb562381086bb6d7a8c62e0debbdcd4
K0Si-003 <hugo.pioda@gmail.com>
2020-07-27 10:25:00 +0200

    add error for login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,7 +7,13 @@ class authController {
       const { token, data } = await User.login(clientPayload.email, clientPayload.password);
       res.status(200).send({ token, data });
     } catch (err) {
-      res.status(400).send({ message: 'invalid credentials' });
+      if (err.message === 'user not found') {
+        res.status(400).send({ message: 'L\'email renseigné n\'est pas valide.'});
+      } else if (err.message === 'incorrect password') {
+        res.status(400).send({ message: 'Le mot de passe n\'est pas valide.'});
+      } else {
+        res.status(400).send({message: 'Une erreur est survenue.'});
+      }
     }
   }
 }
